a3cbef92f1ea1e6271677207e02c20f80db68f8d
statmlben <bendaimath@gmail.com>
2022-03-10 18:06:55 +0800

    Mar10|add decay

--- sim.py
+++ sim.py
@@ -3,24 +3,30 @@ import torch
 from rankseg import rank_dice
 from metrics import dice_coeff
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import matplotlib.patches as mpatch
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # simulation
 # sample_size, width, height, prob_type = 1000, 28, 28, 'exp'
-def sim(sample_size=1000, width=28, height=28, prob_type='exp'):
+def sim(base_=1.01, sample_size=1000, width=28, height=28, prob_type='exp'):
     num_class = 1
     dim = width * height
 
     ## produce prob matrix 
     if prob_type == 'exp':
-        base_power = 1.7
         index_mat = torch.arange(width, device='cuda').repeat(height, 1)
         index_mat = index_mat + index_mat.T
-        prob = base_power ** (-index_mat)
-    elif prob_type == 'unif':
-        prob = .4*torch.ones((width, height), device='cuda')
+        prob = base_ ** (-index_mat)
+    elif prob_type == 'linear':
+        index_mat = torch.arange(width, device='cuda').repeat(height, 1)
+        index_mat = index_mat + index_mat.T
+        prob = 1. - 1./dim*base_*(index_mat)
+        prob = torch.where(prob < 0, 0, prob)
     prob = prob.view(num_class, width, height)
 
     ## generate sample
@@ -34,14 +40,37 @@ def sim(sample_size=1000, width=28, height=28, prob_type='exp'):
     score_rank = dice_coeff(predict.repeat(sample_size, 1, 1, 1), target)
     score_T = dice_coeff(predict_T.repeat(sample_size, 1, 1, 1), target)
 
-    return [score_rank, score_T, predict, predict_T]
+    return [prob, score_rank, score_T, predict, predict_T]
 
 sample_size = 500
 for prob_type in ['exp']:
-    for width in [28, 64, 128, 256]:
-    # for width in [28]:
-        height = width
-        score_rank, score_T, pred_rd, pred_T = sim(sample_size=sample_size, width=width, height=height, prob_type=prob_type)
-        print('#'*20)
-        print('prob_type: %s; width: %d; height: %d' %(prob_type, width, height))
-        print('dice score: rankdice: %.3f; T: %.3f' %(score_rank.mean(), score_T.mean()))
+    for base_ in [1.01, 1.05, 1.1]:
+        for width in [28, 64, 128, 256]:
+            height = width
+            prob, score_rank, score_T, pred_rd, pred_T = sim(base_=base_, sample_size=sample_size, width=width, height=height, prob_type=prob_type)
+            print('#'*20)
+            print('prob_type: %s; width: %d; height: %d' %(prob_type, width, height))
+            print('dice score: rankdice: %.3f(%.3f); T: %.3f(%.3f)' %(score_rank.mean(), score_rank.std()/np.sqrt(sample_size), score_T.mean(), score_T.std()/np.sqrt(sample_size)))
+
+        # figure(figsize=(20, 15), dpi=100)
+        # ax = sns.heatmap(prob[0].cpu(), vmin=0, vmax=1, cmap="YlGnBu")
+        # ax.set_xticks([])
+        # ax.set_yticks([])
+
+        # ## add box
+        # rectangles = {'28x28' : mpatch.Rectangle((0,0), 28, 28, linewidth=2, edgecolor='lightsteelblue', facecolor='none'),
+        #             '64x64' : mpatch.Rectangle((0,0), 64, 64, linewidth=2, edgecolor='cornflowerblue', facecolor='none'),
+        #             '128x128' : mpatch.Rectangle((0,0), 128, 128, linewidth=2, edgecolor='royalblue', facecolor='none'),
+        #             '256x256' : mpatch.Rectangle((0,0), 255.5, 255.5, linewidth=2, edgecolor='darkblue', facecolor='none'),
+        #             }
+
+        # for r in rectangles:
+        #     ax.add_artist(rectangles[r])
+        #     rx, ry = rectangles[r].get_xy()
+        #     cx = rx + rectangles[r].get_width() - 12
+        #     cy = ry + rectangles[r].get_height() - 4
+
+        #     ax.annotate(r, (cx, cy), color='grey', weight='bold', 
+        #                 fontsize=15, ha='center', va='center')
+        # plt.title('decay pattern: %s(%.3f)' %(prob_type, base_), fontsize=20)
+        # plt.show()
